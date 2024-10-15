0ccf92e5ceeed12c75206b3ae74e93604ad689d2
Navdipsing Rajpurohit <navdipsing.r@sphinxworldbiz.com>
2024-10-15 12:34:25 +0530

    second commit

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import GitHub from "../../public/github.svg";
 const Footer: React.FC = () => {
   return (
     <div className="bg-gray-100 mt-auto">
-      <footer className="container">
+      {/* <footer className="container">
         <div className=" flex flex-row justify-between py-3 text-sm">
           <div className="inline-flex items-center">
             <CloudIcon className="w-5 h-5 mr-1.5" />
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
             </a>
           </div>
         </div>
-      </footer>
+      </footer> */}
     </div>
   );
 };
